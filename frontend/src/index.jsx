c2f591f5d42577cc3bda70fc55102815b82f3588
Alekcandr Eremin <eremin.alek2014@yandex.ru>
2025-05-01 00:32:45 +0300

    'fix'

---
+++ frontend/src/index.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import ReactDOM from 'react-dom/client';
+import init from './init.jsx';
+
+const app = async () => {
+  const root = ReactDOM.createRoot(document.querySelector('#chat'));
+  root.render(await init());
+};
+
+app();
